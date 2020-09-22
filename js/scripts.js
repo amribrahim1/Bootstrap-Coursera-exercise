26eb8ad90fb9f1ddea4c0ab52112e056ce066fa6
Amr Eraky <amribrahim1@users.noreply.github.com>
2020-09-22 08:21:16 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(function () {
+    $("#mycarousel").carousel( { interval: 2000 } );
+});
+
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+$("#loginButton").click(function(){
+    $('#loginModal').modal('show')
+});
+$("#reserve-button").click(function(){
+    $('#reserveTable').modal('show')
+})
